6ad3f8325dfeecb1a047cf1e799489e30a4e4c11
Fyodor Danilov <danilovfa@protonmail.com>
2024-09-26 13:28:30 +0300

    Initial commit

---
+++ backend/src/main/kotlin/com/example/plugins/HTTP.kt
@@ -0,0 +1,14 @@
+package com.example.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureHTTP() {
+    routing {
+        swaggerUI(path = "openapi")
+    }
+    install(Compression)
+}
